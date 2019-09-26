a26c36f1b30e80a494fbde37f1daee67e989e530
markusohlen <mo223dg@student.lnu.se>
2019-09-26 11:02:56 +0200

    Checking if username and password is correct

--- model/LoginModel.php
+++ model/LoginModel.php
@@ -2,5 +2,39 @@
 
 class LoginModel
 {
-    
+    private $usernames = array();
+
+    public function __construct() {
+        $obj = new stdClass;
+        $obj->username = "Admin";
+        $obj->password = "Password";
+
+        $obj2 = new stdClass;
+        $obj2->username = "abc";
+        $obj2->password = "123123";
+
+        array_push($this->usernames, $obj, $obj2);
+    }
+    public function usernameExists($username) {
+        foreach ($this->usernames as $user) {
+            if ($user->username === $username) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function checkUsernameAndPassword($username, $password) {
+        $user;
+        foreach ($this->usernames as $u) {
+            if ($u->username === $username) {
+                $user = $u;
+            }
+        }
+
+        if ($user->password === $password) {
+            return true;
+        }
+        return false;
+    }
 }
